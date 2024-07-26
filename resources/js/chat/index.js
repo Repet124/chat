a67b24c0170768dd80254373d790aafcaa7295e9
Oleg Bondarenko <hlop4ik124@gmail.com>
2024-07-26 19:20:31 +0300

    spa ready

---
+++ resources/js/chat/index.js
@@ -0,0 +1,5 @@
+import '@/bootstrap.js';
+import { createApp, ref } from 'vue'
+import App from './components/App.vue';
+
+createApp(App).mount('#app');
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
+
+Route::get('/chat{any}', function () {
+	return view('chat');
+})->where('any', '.*');
