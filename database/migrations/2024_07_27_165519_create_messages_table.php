1ad4ec441786af1fb75b82af6751dc3f1dd27de5
Oleg Bondarenko <hlop4ik124@gmail.com>
2024-07-27 21:39:41 +0300

    migrations

--- database/migrations/2024_07_27_165519_create_messages_table.php
+++ database/migrations/2024_07_27_165519_create_messages_table.php
@@ -11,12 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('messages', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->string('text', 255);
+            $table->foreignId('user_id')->constrained();
         });
     }
 
@@ -25,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('messages');
     }
 };
--- resources/js/chat/components/Chat.vue
+++ resources/js/chat/components/Chat.vue
@@ -3,7 +3,7 @@
 		<Message>Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Officiis dolore, aliquid quo, vero harum, error perspiciatis praesentium provident dignissimos vel officia illo repellat ullam explicabo odit, expedita deleniti incidunt tempora.</Message>
 		<Message my>Test</Message>
 		<Message my>Lorem ipsum dolor sit, amet, consectetur adipisicing elit. Quo, et!</Message>
-		<Message>Lorem ipsum dolor sit, amet, consectetur adipisicing elit. Quo, et!</Message>
+		<Message>consectetur adipisicing elit Lorem ipsum dolor sit, amet, consectetur adipisicing elit Lorem ipsum dolor sit, amet, consectetur adipisicing elit</Message>
 	</div>
 </template>
 
