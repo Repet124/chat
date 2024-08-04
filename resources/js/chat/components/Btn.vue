0f8954223718eb4af2d8fdec9954691112f7f698
Oleg Bondarenko <hlop4ik124@gmail.com>
2024-08-04 15:53:24 +0300

    Btn component

---
+++ resources/js/chat/components/Btn.vue
@@ -0,0 +1,15 @@
+<template>
+	<button
+		class="py-2 px-4 font-bold flex items-center justify-center w-max text-xl border-none"
+		:class="[
+			bg || 'bg-blue-400 hover:bg-blue-300',
+			color || 'text-white'
+		]"
+	>
+		<slot />
+	</button>
+</template>
+
+<script setup>
+	defineProps(['bg', 'color'])
+</script>
